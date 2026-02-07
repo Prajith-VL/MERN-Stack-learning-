fc95f30e93915a492fafa0791763a92b00d03172
Prajith-VL <prajith.loganathan06@gmail.com>
2026-02-07 10:54:15 +0530

    another type of function declaration

--- variables.js
+++ variables.js
@@ -5,7 +5,12 @@ function getname (){ //fuction with normal declaration
     console.log('learning javascipt to MERN stack')
 }
 
+//var getname2 = () =>{ //fuction declared as a variable
+//        console.log('learning javascipt to MERN stack');
+//}
 getname();// function call
+
+//console.log(getname2())
 
 var a = 100; //variable declaration
 console.log(a);//printing
