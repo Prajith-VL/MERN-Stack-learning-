ee929a5e1b286b7e2fa4737d46b14be32c65e053
Prajith-VL <prajith.loganathan06@gmail.com>
2026-02-07 10:47:58 +0530

    first commit of javascript variables and fuction

---
+++ variables.js
@@ -0,0 +1,15 @@
+console.log(a); //hoisting variable
+console.log(getname);//hoisting function
+
+function getname (){ //fuction with normal declaration
+    console.log('learning javascipt to MERN stack')
+}
+
+getname();// function call
+
+var a = 100; //variable declaration
+console.log(a);//printing
+
+a= 'java learning';
+console.log(a);
+
