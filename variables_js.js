c8e4b9f313629e2d937074641eca28ae29de9948
Prajith-VL <prajith.loganathan06@gmail.com>
2026-02-23 13:05:17 +0530

    learned variables and interger data type

---
+++ variables_js.js
@@ -0,0 +1,19 @@
+//variables= containers to store a value
+
+//let x; =>declaration
+//x=1223; => Assignment
+
+let a;
+a = 22;
+
+console.log(a);//displays
+
+let b=34567;//both at same time
+
+
+let age=55;
+let price = 333.65;
+
+console.log(`You are ${age} years old`);//to dispaly variable 
+
+console.log(typeof price);// to identify the data type
