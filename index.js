7414d5b301bb11846d3039efbc6e54ce2ac1f66d
Prajith-VL <prajith.loganathan06@gmail.com>
2026-02-23 12:48:33 +0530

    learned about comment and how to access content of html using java script

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// to create a comment
+/* to create
+a multi line comment*/
+
+/*
 //for dispalying
 console.log(`hei everyone iam prajith`);
 
 //for a alert message in a window
 window.alert(`This is a lert that iam learning javascript`);
+*/
+
+document.getElementById("myH1").textContent="Hello";
+document.getElementById("myP").textContent=`hi i have given content to my html page using js`;
