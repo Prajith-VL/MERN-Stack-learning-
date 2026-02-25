fb3b1323b9bc9af5591f50817a8f92bd686e6115
Prajith-VL <prajith.loganathan06@gmail.com>
2026-02-25 21:12:10 +0530

    user input

---
+++ user_input.js
@@ -0,0 +1,16 @@
+//How to access user input.
+
+/* there are 2 ways
+    1.Window prompt
+    2.HTML text box
+*/
+
+//Window prompt
+let username=window.prompt(`What's your username:`);
+console.log(username);
+
+//Html text box
+let user;
+document.getElementById("submit button").onclick=function(){
+    user=document.getElementById("mytext").value;
+}
