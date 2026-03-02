42728929efa680b61b386d4a770ecc142b7a3875
Prajith-VL <prajith.loganathan06@gmail.com>
2026-03-02 12:42:31 +0530

    user input through html

--- user_input.js
+++ user_input.js
@@ -13,5 +13,5 @@ console.log(username);
 let user;
 document.getElementById("submit button").onclick=function(){
     user=document.getElementById("mytext").value;
-    document.getElementById("myH1").textContent=`Hello ${user}`;
+    document.getElementById("myH1").textContent="Hello";
 }
